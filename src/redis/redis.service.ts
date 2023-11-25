a637154e6bd757f301a202b28494fe695a8a77a5
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-11-25 01:19:06 -0300

    Redis Config

---
+++ src/redis/redis.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisService {
+  private readonly redis: Redis;
+
+  constructor() {
+    this.redis = new Redis();
+  }
+
+  getClient() {
+    return this.redis;
+  }
+}
