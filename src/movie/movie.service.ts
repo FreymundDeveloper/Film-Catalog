69ffa2bde9ed1756382e1466eed17a6cf6dc8389
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-11-25 02:14:29 -0300

    Movie Service(Module) Ajustment

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MovieEntity } from './movie.entity';
 
 @Injectable()
 export class MovieService {
+  constructor(
+    @InjectRepository(MovieEntity)
+    private readonly movieRepository: Repository<MovieEntity>,
+  ) {}
+
   getAllMovies() {
     return 'Get all movies from service';
   }
